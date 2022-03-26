c7613a52363fc9c81fcacaa318fe5fe24c3138d0
Dell <Dell@DESKTOP-UCFLIOD>
2022-03-26 17:07:08 +0100

    Add project files.

---
+++ BSKSzyfrogramy/CipherService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSKSzyfrogramy
+{
+    public static class CipherService
+    {
+        public static string CipherRailFence(string ciphertext, int rowCount)
+        {
+            char[,] array = new char[ciphertext.Length, rowCount];
+            int increment = 1;
+            int row = 0;
+
+            for (int col = 0; col < ciphertext.Length; col++)
+            {
+                array[col, row] = ciphertext[col];
+
+                if (row == rowCount - 1) increment = -1;
+                else if (row == 0) increment = 1;
+                
+                row += increment;
+            }
+
+            string result = "";
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < ciphertext.Length; j++)
+                {
+                    char c = array[j, i];
+                    if(c != '\0')
+                    {
+                        result += c;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static string DecipherRailFence(string cipher, int rowCount)
+        {
+            
+        }
+
+        public static string MatrixTransp(string ciphertext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string MatrixTransp_B(string ciphertext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BSKSzyfrogramy/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using BSKSzyfrogramy;
+
+string ciphertext = Console.ReadLine();
+
+Console.WriteLine(CipherService.CipherRailFence(ciphertext, 3));
